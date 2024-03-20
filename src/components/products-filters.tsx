4039e62e26086fd62a0abb022ea94956816cca33
Leandro <lleandrofr@gmail.com>
2024-03-20 15:06:51 -0300

    feat: setup hook form and validations for products filter

--- src/components/products-filters.tsx
+++ src/components/products-filters.tsx
@@ -1,13 +1,34 @@
 import { Search } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
+const productsFiltersSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+});
+
+type ProductsFiltersSchema = z.infer<typeof productsFiltersSchema>;
+
 export function ProductsFilters() {
+  const { register, handleSubmit } = useForm<ProductsFiltersSchema>({
+    resolver: zodResolver(productsFiltersSchema),
+  });
+
+  const handleFilterProducts = (data: ProductsFiltersSchema) => {
+    console.log(data);
+  };
+
   return (
-    <form className="flex items-center gap-2">
-      <Input name="id" placeholder="ID do produto" />
-      <Input name="name" placeholder="Nome do produto" />
+    <form
+      onSubmit={handleSubmit(handleFilterProducts)}
+      className="flex items-center gap-2"
+    >
+      <Input placeholder="ID do produto" {...register("id")} />
+      <Input placeholder="Nome do produto" {...register("name")} />
       <Button type="submit" variant="link">
         <Search className="size-4 mr-2" />
         Filtrar resultados
