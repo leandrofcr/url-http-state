252aeafa059cf188437e6b04f754d144607dff02
Leandro <lleandrofr@gmail.com>
2024-03-20 15:19:04 -0300

    fix(product0-table): add missing table row

--- src/components/products-table.tsx
+++ src/components/products-table.tsx
@@ -1,30 +1,31 @@
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table";
-  
-  export function ProductsTable() {
-    return (
-      <Table>
-        <TableHeader>
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export function ProductsTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
           <TableHead>ID</TableHead>
           <TableHead>Produto</TableHead>
           <TableHead>Preço</TableHead>
-        </TableHeader>
-        <TableBody>
-          {Array.from({ length: 10 }).map((_, i) => (
-            <TableRow key={i}>
-              <TableCell>82s8sdsd213s</TableCell>
-              <TableCell>Produto {i}</TableCell>
-              <TableCell>R$ 198,00</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    );
-  }
-  
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {Array.from({ length: 10 }).map((_, i) => (
+          <TableRow key={i}>
+            <TableCell>82s8sdsd213s</TableCell>
+            <TableCell>Produto {i}</TableCell>
+            <TableCell>R$ 198,00</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
