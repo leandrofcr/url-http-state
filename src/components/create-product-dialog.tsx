a8f6c5b41d30eeb40b5bdd160cb2067c53c29dad
Leandro <lleandrofr@gmail.com>
2024-03-20 15:20:08 -0300

    feat: setup react hook form for the create product form

--- src/components/create-product-dialog.tsx
+++ src/components/create-product-dialog.tsx
@@ -1,4 +1,11 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 import {
   DialogClose,
   DialogContent,
@@ -7,37 +14,50 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+
+const createProductsSchema = z.object({
+  name: z.string(),
+  price: z.coerce.number(),
+});
+
+type CreateProductsSchema = z.infer<typeof createProductsSchema>;
 
 export function CreateProductDialog() {
+  const { register, handleSubmit } = useForm<CreateProductsSchema>({
+    resolver: zodResolver(createProductsSchema),
+  });
+
+  const handleCreateProduct = (data: CreateProductsSchema) => {
+    console.log(data);
+  };
+
   return (
     <DialogContent>
       <DialogHeader>
         <DialogTitle>Novo produto</DialogTitle>
         <DialogDescription>Criar novo produto no sistema</DialogDescription>
-
-        <form action="" className="space-y-6">
-          <div className="grid grid-cols-4 items-center text-right gap-3">
-            <Label htmlFor="name">Produto</Label>
-            <Input className="col-span-3" id="name" />
-          </div>
-
-          <div className="grid grid-cols-4 items-center text-right gap-3">
-            <Label htmlFor="price">Preço</Label>
-            <Input className="col-span-3" id="price" />
-          </div>
-
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button type="button" variant="outline">
-                Cancelar
-              </Button>
-            </DialogClose>
-            <Button type="submit">Salvar</Button>
-          </DialogFooter>
-        </form>
       </DialogHeader>
+
+      <form onSubmit={handleSubmit(handleCreateProduct)} className="space-y-6">
+        <div className="grid grid-cols-4 items-center text-right gap-3">
+          <Label htmlFor="name">Produto</Label>
+          <Input className="col-span-3" id="name" {...register("name")} />
+        </div>
+
+        <div className="grid grid-cols-4 items-center text-right gap-3">
+          <Label htmlFor="price">Preço</Label>
+          <Input className="col-span-3" id="price" {...register("price")} />
+        </div>
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant="outline">
+              Cancelar
+            </Button>
+          </DialogClose>
+          <Button type="submit">Salvar</Button>
+        </DialogFooter>
+      </form>
     </DialogContent>
   );
 }
